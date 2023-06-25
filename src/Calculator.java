5bffcc90b100a06ae399d1dabcd10708f088e457
Answer795031 <a.n.s.w.e.r130597@gmail.com>
2023-06-25 17:41:20 +0700

    Переход от int к double

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,29 +3,45 @@ public class Calculator {
     static Scanner scanner = new Scanner(System.in);    // Объявляем метод Scanner для ввода значений в консоль
 
     public static void main(String[] args) {
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation = getOperation();
-        int result = calc(num1, num2, operation);
+        /*
+        Метод main() присваивает переменным num1 и num2 значения, считанные в методе getDouble(), а так же переменной
+        operation значение, считанное в методе getOperation(), после чего передает три значения методу calc().
+        Метод calc() возвращает значение result в метод main(), посчле чего происходит вывод результата в консоль.
+         */
+        double num1 = getDouble();                      // Запись первого числа
+        double num2 = getDouble();                      // Запись второго числа
+        char operation = getOperation();                // Запись операции
+        double result = calc(num1, num2, operation);    // Запись результата
         System.out.println("Результат операции: " + result);
     }
 
-    public static int getInt(){                         // Метод, отвечающий за ввод чисел для вычислений
+    public static double getDouble(){
+        /*
+        Метод считывает с консоли число, проверяет его на соответствие типу double, после чего записывает значение
+        в переменную num. В случае несоответствия входного числа типу double, происходит рекурсия.
+        Метод возвращает переменную num, чтобы в main() мы могли присвоить это значение переменной num1 или num2.
+         */
         System.out.print("Введите число: ");
-        int num;
-        if(scanner.hasNextInt()){                       // Проверка на соответстиве числа типу int
-            num = scanner.nextInt();                    // Записываем число в переменную num
+        double num;
+        if(scanner.hasNextDouble()){
+            num = scanner.nextDouble();
         } else {
             System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();                             // Рекурсия в случае ошибки при вводе числа
-            num = getInt();                             // Повтор
+            scanner.next();
+            num = getDouble();          // Рекурсия
         }
-        return num;                                     // Возвращаем значение num для записи в num1 в main()
+        return num;                                     // Возвращаем значение num для  метода main()
     }
 
-    public static char getOperation(){                  // Метод, отвечающий за выбор операции, производимой над числами
+    public static char getOperation(){
+        /*
+        Метод считывает с консоли символ, проверяет его на соответствие типу char.
+        Необходимо ввести один из символов: '+', '-', '*', '/', иначе в методе calc() возникнет ошибка, после чего
+        программа вызовет рекурсию, попросив повторить ввод команды в getOperation().
+        В случае несоответствия вводного символа типу char, происходит рекурсия.
+         */
         System.out.print("Введите операцию: ");
-        char operation;                                 // Зададим 4 операции типом char: '+', '-', '*', '/'
+        char operation;
         if(scanner.hasNext()){
             operation = scanner.next().charAt(0);
         } else {
@@ -36,8 +52,15 @@ public class Calculator {
         return operation;                               // Возвращаем значение operation для метода main()
     }
 
-    public static int calc(int num1, int num2, char operation){     // Метод, отвечающий непосредственно за вычисления
-        int result;
+    public static double calc(double num1, double num2, char operation){
+        /*
+        Метод, отвечающий за вычисления над переменными num1 и num2 в соответствии с требуемой операцией.
+        Для корректной работы метода необходимы переменные num1 и num2 типа double, а так же знак математической
+        операции типа char ('+', '-', '*', '/').
+        В случе если в методе getOperation() был считан символ char, отличающийся от требуемых, возникает рекурсия.
+         */
+
+        double result;
         switch (operation){
             case '+':
                 result = num1 + num2;
